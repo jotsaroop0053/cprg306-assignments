21096d4e7bf84ae26f0dd881494645c71bc529da
Jotsaroop Singh Nagpal <141177591+jotsaroop0053@users.noreply.github.com>
2024-01-16 13:01:55 -0700

    Added files

---
+++ cprg306-assignments/app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Jotsaroop Singh Nagpal</p>
+        <Link href="https://github.com/jotsaroop0053"><p>This is my github profile</p></Link>
+      </main>
+    );
+  }
