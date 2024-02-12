eca2bde9c8a19720ae5da5e8e7fae894b6bbec82
Jotsaroop Singh Nagpal <141177591+jotsaroop0053@users.noreply.github.com>
2024-02-12 12:43:34 -0700

    week-4 Done

--- app/page.js
+++ app/page.js
@@ -2,9 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items- p-24">
-      <Link href="./week-2">Week-2</Link>
-      <Link href="./week-3">Week-3</Link>
+    <main className="flex min-h-screen flex-col items- p-24 ">
+      <Link href="./week-2">Week-2 Assignment</Link>
+      <Link href="./week-3">Week-3 Assignment</Link>
+      <Link href="./week-4">Week-4 Assignment</Link>
     </main>
   )
 }
---
+++ app/week-4/page.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import NewItem from './new-item';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Page Component</h1>
+      <NewItem />
+    </div>
+  );
+};
+
+export default Page;
