bb2e2c2368c3135334acfa780319da4a96ee397a
Jotsaroop Singh Nagpal <141177591+jotsaroop0053@users.noreply.github.com>
2024-03-11 13:02:23 -0600

    week-7

---
+++ app/week-7/item-list.js
@@ -0,0 +1,54 @@
+"use client";
+
+import React, { useState } from 'react';
+import Item from './item';
+
+const ItemList = ({ items, onItemSelect }) => {
+    const [sortBy, setSortBy] = useState('name'); 
+    
+    const sortedItems = [...items].sort((a, b) => {
+      if (sortBy === 'name') {
+        return a.name.localeCompare(b.name);
+      } else if (sortBy === 'category') {
+        return a.category.localeCompare(b.category);
+      }
+      
+      return 0;
+    });
+  
+    
+    const handleSortByName = () => setSortBy('name');
+    const handleSortByCategory = () => setSortBy('category');
+  
+    return (
+      <div>
+        <button
+          onClick={handleSortByName}
+          style={{ backgroundColor: sortBy === 'name' ? 'green' : 'white' }}
+        >
+          Sort by Name
+        </button>
+        <button
+          onClick={handleSortByCategory}
+          style={{ backgroundColor: sortBy === 'category' ? 'green' : 'white' }}
+        >
+          Sort by Category
+        </button>
+  
+       
+        <ul>
+          {sortedItems.map(item => (
+            <Item 
+              key={item.name} 
+              name={item.name} 
+              quantity={item.quantity} 
+              category={item.category} 
+              onSelect={() => onItemSelect(item)} // Pass onSelect prop to Item component
+            />
+          ))}
+        </ul>
+      </div>
+    );
+};
+
+export default ItemList;
